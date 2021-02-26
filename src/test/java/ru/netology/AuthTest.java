6e1897bc4784ddb5b37b7dd493da0533c0909fba
Evgeny Vasilev <jony.torrent@gmail.com>
2021-02-26 12:36:45 +0300

    fixes+

--- src/test/java/ru/netology/AuthTest.java
+++ src/test/java/ru/netology/AuthTest.java
@@ -1,81 +1,52 @@
 package ru.netology;
-
-import com.codeborne.selenide.Condition;
 import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.DataGenerator.Registration.SendOnServer.generateUser;
-
-
+import static ru.netology.DataGenerator.Registration.*;
 class AuthTest {
     @BeforeEach
-    public void setUpAll() {
+    public void setUp() {
         open("http://localhost:9999/");
     }
-
     @Test
     void shouldPassWithActiveUser() {
-        val user = generateUser("Active");
-
-
-
-        $("[name=login]").setValue(user.getLogin());
-        $("[name=password]").setValue(user.getPassword());
+        val validUser = generateUser("active");
+        $("[name=login]").setValue(validUser.getLogin());
+        $("[name=password]").setValue(validUser.getPassword());
         $("[data-test-id=action-login]").click();
-        $(withText("Личный кабинет")).shouldBe(Condition.visible);
+        $(withText("Личный кабинет")).shouldBe(visible);
     }
-
-
-
-
-
-
-
-
-    /*@Test
-    void shouldPassWithActiveUser() {
-        RegistrationInfo dataOrderCard= DataGenerator.Registration.generateActiveUser();
-        DataGenerator.SendOnServer.setUpAll(dataOrderCard);
-        $("[name=login]").setValue(dataOrderCard.getLogin());
-        $("[name=password]").setValue(dataOrderCard.getPassword());
-        $("[data-test-id=action-login]").click();
-        $(withText("Личный кабинет")).shouldBe(Condition.visible);
-    }*/
-
-    /*@Test
+    @Test
     void shouldPassWithBlockedUser() {
-        RegistrationInfo dataOrderCard= DataGenerator.Registration.generateBlockedUser();
-        DataGenerator.SendOnServer.setUpAll(dataOrderCard);
-        $("[name=login]").setValue(dataOrderCard.getLogin());
-        $("[name=password]").setValue(dataOrderCard.getPassword());
+        val blockedUser = generateUser("blocked");
+        $("[name=login]").setValue(blockedUser.getLogin());
+        $("[name=password]").setValue(blockedUser.getPassword());
         $("[data-test-id=action-login]").click();
-        $(withText("Пользователь заблокирован")).shouldBe(Condition.visible);
+        $(withText("Пользователь заблокирован")).shouldBe(visible);
     }
-
     @Test
     void shouldFailWithInvalidLogin() {
-        RegistrationInfo dataOrderCard= DataGenerator.Registration.generateActiveUser();
-        DataGenerator.SendOnServer.setUpAll(dataOrderCard);
-
-        $("[name=login]").setValue("12345");
-        $("[name=password]").setValue(dataOrderCard.getPassword());
+        val wrongLoginUser = generateWrongLoginUser("active");
+        $("[name=login]").setValue(wrongLoginUser.getLogin());
+        $("[name=password]").setValue(wrongLoginUser.getPassword());
         $("[data-test-id=action-login]").click();
-        $(withText("Неверно указан логин")).shouldBe(Condition.visible);
+        $(withText("Неверно указан логин или пароль")).shouldBe(visible);
     }
-
     @Test
     void shouldFailWithInvalidPassword() {
-        RegistrationInfo dataOrderCard= DataGenerator.Registration.generateActiveUser();
-        DataGenerator.SendOnServer.setUpAll(dataOrderCard);
-        $("[name=login]").setValue(dataOrderCard.getLogin());
-        $("[name=password]").setValue("qwerty");
+        val wrongLoginUser = generateWrongPasswordUser("active");
+        $("[name=login]").setValue(wrongLoginUser.getLogin());
+        $("[name=password]").setValue(wrongLoginUser.getPassword());
         $("[data-test-id=action-login]").click();
-        $(withText("Неверно указан логин или пароль")).shouldBe(Condition.visible);
-    }*/
-
-
+        $(withText("Неверно указан логин или пароль")).shouldBe(visible);
+    }
 }
+
+
+
+
+
